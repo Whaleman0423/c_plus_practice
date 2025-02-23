b066260cdc06450446c13ccc004e5ff4faee4b00
Whaleman0423 <sheiyuray@gmail.com>
2025-02-23 14:44:02 +0800

    first commit

--- ntu_cool_class_basic/2.8/namespace_example.cpp
+++ ntu_cool_class_basic/2.8/namespace_example.cpp
@@ -7,9 +7,13 @@ namespace a {
 int b = 5;
 
 int main(){
+    int b = 7;
+    {
+        int b = 9;    
+    }
 
     std::cout << "a::b：" << a::b << std::endl // 3
-              << "b   ：" << b    << std::endl // 5
+              << "b   ：" << b    << std::endl // 7
               << "::b ：" << ::b  << std::endl; // 5
               
     return 0;
